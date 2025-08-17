bb7cddc8213c753ee7142b307d1d511714594097
SejongPark <lifedesigner88@gmail.com>
2025-08-17 16:12:20 +0900

    5.2 Custom Tools

--- A01_News_Reader_Agent/main.py
+++ A01_News_Reader_Agent/main.py
@@ -1,4 +1,62 @@
 import dotenv
 
-dotenv.load_dotenv()
+dotenv.load_dotenv() # 환경변수 세팅
 
+
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+from tools import count_letters
+
+@CrewBase
+class TrnslatorCrew:
+
+    @agent
+    def translator_agent(self):
+        return Agent(
+            config=self.agents_config["translator_agent"]
+        )
+
+    @agent
+    def counter_agent(self):
+        return Agent(
+            config=self.agents_config["counter_agent"],
+            tools=[count_letters]
+        )
+
+    @task
+    def translate_task(self):
+        return Task(
+            config=self.tasks_config["translate_task"]
+        )
+
+    @task
+    def retranslate_task(self):
+        return Task(
+            config=self.tasks_config["retranslate_task"]
+        )
+
+    @task
+    def reretranslate_task(self):
+        return Task(
+            config=self.tasks_config["reretranslate_task"]
+        )
+
+    @task
+    def count_task(self):
+        return Task(
+            config=self.tasks_config["count_task"],
+        )
+
+    @crew
+    def assemble_crew(self):
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            verbose=True
+        )
+
+TrnslatorCrew().assemble_crew().kickoff(
+    inputs={
+        "sentence" : "I'm Nico I like to ried my bicicle in Napoli"
+    }
+)
