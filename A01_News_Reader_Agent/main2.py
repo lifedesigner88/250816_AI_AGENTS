a4710087e86e1940e76619ab3ee7dcdb8547e1b9
SejongPark <lifedesigner88@gmail.com>
2025-09-30 16:38:46 +0900

    3.1 Your First CrewAi Agent

---
+++ A01_News_Reader_Agent/main2.py
@@ -0,0 +1,42 @@
+
+import dotenv
+dotenv.load_dotenv()
+
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+
+@CrewBase
+class TranslatorCrew:
+
+    @agent
+    def translator_agent(self):
+        return Agent(
+            config=self.agents_config["translator_agent"]
+        )
+
+    @task
+    def translate_task(self):
+        return Task(
+            config=self.tasks_config["translate_task"]
+        )
+
+    @task
+    def retranslate_task(self):
+        return Task(
+            config=self.tasks_config["retranslate_task"]
+        )
+
+    @crew
+    def assemble_crew(self):
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            verbose=True,
+        )
+
+
+TranslatorCrew().assemble_crew().kickoff(
+    inputs={
+        "sentence": "나는 세종이야. 서울에 살고 있어."
+    }
+)
