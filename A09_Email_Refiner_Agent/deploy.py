b006477f500e68b0a42b0004454a811aaab8f572
SejongPark <lifedesigner88@gmail.com>
2025-10-09 15:22:26 +0900

    12.7 Deploy - 1

---
+++ A09_Email_Refiner_Agent/deploy.py
@@ -0,0 +1,36 @@
+import vertexai, dotenv, os
+dotenv.load_dotenv()
+
+from vertexai.preview import reasoning_engines
+
+from travel_advisor_agent.agent import travel_advisor_agent
+
+PROJECT_ID = "sejongclass"
+LOCATION = "asia-northeast1"
+BUCKET = "gs://weather_agent_88"
+
+vertexai.init(
+    project=PROJECT_ID,
+    location=LOCATION,
+    staging_bucket=BUCKET,
+)
+
+app = reasoning_engines.AdkApp(
+    agent=travel_advisor_agent,
+    enable_tracing=True,
+)
+
+remote_app = vertexai.agent_engines.create(
+    display_name="Travel Advisor Agent",
+    agent_engine=app,
+    requirements=[
+        "google-cloud-aiplatform[adk,agent-engines]",
+        "litellm",
+    ],
+    extra_packages=[
+        "travel_advisor_agent"
+    ],
+    env_vars={
+        "OPENAI_API_KEY": os.environ["OPENAI_API_KEY"],
+    }
+)
